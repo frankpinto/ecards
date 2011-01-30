41e663c7a97844c5ddc204bbb07afa5743d35a30
Frank Pinto <frank@TheWind.(none)>
2011-01-30 02:31:09 -0500

    Added listing friends for testing

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source :rubygems
 gem 'rails', '2.3.8'
+gem 'mongrel', '1.1.5'
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,11 +1,15 @@
 class AuthController < ApplicationController
   def login
+    session['fbook'] = params['access_token']
+    session['logged_in'] = true
   end
 
   def logout
   end
 
   def index
+    @access_token = session['fbook'] || 192551997440118
+    @logged_in = session['logged_in'] || false;
   end
 
 end
