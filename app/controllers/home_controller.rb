8cd0a215285c95c1a6c7c1611513caf0c2c2f4b1
Frank Pinto <frank@TheWind.(none)>
2011-02-04 03:22:53 -0500

    Birfdays was the best days\!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def list
     if user_token?
-      @graph = Koala::Facebook::GraphAPI.new self.user_token
-      @friends = @graph.get_connections('me', 'friends', :q => 'Alden')
-      render :text => @friends.inspect
+      #@graph = Koala::Facebook::GraphAPI.new self.user_token
+      @graph = Koala::Facebook::GraphAndRestAPI.new(self.user_token)
+      @me = @graph.get_object('me', :fields => 'birthday, first_name, last_name')
+      @friends = @graph.graph_call('me/friends', :fields => 'birthday, name')['data'].select {|friend| friend.has_key? 'birthday'}
     else
       @friends = ["#{self.token}"]
     end
