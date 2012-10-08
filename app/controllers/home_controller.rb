cdeeaade0ed095a17dc101d41ac2efb1204fa314
Frank Pinto <frank@onswipe.com>
2012-10-08 02:10:59 -0400

    Project half done but maybe discontinued. Cleanup later

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,24 @@ class HomeController < ApplicationController
       @friends = ["#{self.token}"]
     end
   end
+
+  def fbevent
+    if user_token?
+      @graph = Koala::Facebook::GraphAPI.new(self.user_token)
+
+      # First get people that are invited to the event and collect their name
+      @friends = @graph.get_connections('199582233391970', 'invited')
+      @names = @friends.collect {|friend| friend['name']}
+      f = File.open 'names.txt', 'w'
+      f.puts @names.join("\n")
+      f.close
+
+      # Then run them through the Columbia UNI Lookup and pull their emails
+      #@names.each do |name|
+      #end
+    else
+      render :text => 'login first'
+    end
+  end
+  
 end
